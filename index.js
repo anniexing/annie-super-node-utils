66096a4a038d4c01ca78a9fbad62e57f8c493302
Liangxiong Zhu <lxzhu@outlook.com>
2018-08-10 21:07:23 +0200

    add rand and hash to index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var MySqlUtils = require("./mysql-utils.js");
 var PugEngine = require("./pug-engine.js");
+var rand = require("./rand.js");
+var hash = require("./hash.js");
 module.exports={
   createMySql:function(options){
     return new MySqlUtils(options);
   },
   createPug:function(options){
     return new PugEngine(options);
-  }
+  },
+  rand:rand,
+  hash:hash
 };
