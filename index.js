9354699828fc21761c2211237b6f22e51808b302
Liangxiong Zhu <lxzhu@outlook.com>
2018-08-11 10:54:39 +0200

    expose email-address-validator

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ var hash = require("./hash.js");
 var staticFile = require("./static-file.js");
 var markdown = require("./markdown.js");
 var httpCode = require("./httpCode.js")
+var EmailAddressValidator = require("./email-address-validator.js");
+
 module.exports={
   createMySql:function(options){
     return new MySqlUtils(options);
   },
   createPug:function(options){
     return new PugEngine(options);
+  },
+  createEmailAddressValidator:function(pattern){
+  	return new EmailAddressValidator(pattern);
   },
   rand:rand,
   hash:hash,
